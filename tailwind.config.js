10b885c8317f8c328cc5c43cbe715966361603e2
chrisdadev13 <chrisdadev13@gmail.com>
2022-04-18 18:47:34 -0400

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    letterSpacing: {
+      tight: '-.015em'
+    },
+    extend: {
+      width:{
+        'w-media': '81.25vw'
+      },
+      height: {
+        'half-screen': '50vh',
+        'h-media': '115vh'
+      },
+      animation: {
+        'fadeIn-1': 'fade 1s linear',
+        'fadeIn-3': 'fade 3s linear',
+        'border-fadeIn': 'fadeBorder 0.5s ease-in-out',
+      },
+      keyframes: {
+        fade: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 100 },
+        },
+        fadeBorder: {
+          '0%': { borderWidth: '0' },
+          '70%': { borderWidth: '2px',
+                    borderColor: '#60a5fa'
+          },
+          '100%': { borderWidth: '0' }
+        }
+      }
+    },
+  },
+  plugins: [],
+}
