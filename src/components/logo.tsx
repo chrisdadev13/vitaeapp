7980d809495da65dbf832b4580eea3b029b68d40
chrisdadev13 <chrisdadev13@gmail.com>
2022-04-20 15:16:26 -0400

    Minor changes

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,7 +4,7 @@ const Logo: React.FC = () => {
   return(
     <div className="cursor-pointer">
       <h2 className="text-3xl lg:text-2xl text-blue-400 font-semibold">
-        viteapp 
+        vitaepp 
       </h2>
     </div>
   )
