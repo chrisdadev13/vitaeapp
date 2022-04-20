b4104dc6e87eaea18b144ec5568c2586edbb1977
chrisdadev13 <chrisdadev13@gmail.com>
2022-04-19 20:45:34 -0400

    Get user personal data

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,17 +1,33 @@
-import React, { useState } from "react";
+import React, { ChangeEvent, FormEventHandler, useState } from "react";
 import Header from "./Header";
 import CVForm from "./CVForm/CVForm";
 
-const Main = () => {
+const Main: React.FC = () => {
+
+  type Object = {
+    [key: string]: any 
+  }
 
   const [currentPage, setCurrentPage] = useState<number>(1);
   const [currentForm, setCurrentForm] = useState<number>(1);
 
+  const [cv, setCv] = useState<Object>({});
+
+  const handleData: FormEventHandler<HTMLInputElement> = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    setCv({
+      ...cv,
+      [name]: value
+    })
+  }
+
   return(
     <>
-      <Header section={currentPage}/>
-      <CVForm />
-      <button onClick={() => setCurrentPage(currentPage + 1)}>Test button</button>
+      <Header section={currentPage} />
+      <CVForm cvData={cv} handleChange={handleData} />
+      <button onClick={() => console.log(cv)}>Test button</button>
     </>
   )
 }
